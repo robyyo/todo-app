25ebd927da9957a13092f245725efea5418185fb
Robert Finkley <me@robertfinkley.com>
2022-11-08 19:44:07 -0500

    setting up menu button for header w/ storybook

---
+++ components/MenuButton/MenuButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './menubutton.module.css';
+
+const MenuButton = () => {
+  return (
+    <div className={styles.menubuttonWrapper}>
+      <button>MenuButton</button>
+    </div>
+  );
+};
+
+export default MenuButton;
