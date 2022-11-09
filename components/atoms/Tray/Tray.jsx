da9c4d58adc382904a13dda543fe8c552f00cef2
Robert Finkley <me@robertfinkley.com>
2022-11-08 21:51:17 -0500

    working on nav mobile tray amd story

---
+++ components/atoms/Tray/Tray.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './tray.module.css';
+
+const Tray = () => {
+  return <div className={styles.trayWrapper}>Tray</div>;
+};
+
+export default Tray;
---
+++ stories/Components/atoms/Tray.stories.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Tray from '../../../components/atoms/Tray/Tray';
+
+export default {
+  title: 'Components/Atoms/Tray',
+  component: Tray,
+};
+
+export const Default = () => <Tray />
