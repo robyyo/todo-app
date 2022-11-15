d0e1bc6954f05a2ce16f69883826b6dd701cb65b
Robert Finkley <me@robertfinkley.com>
2022-11-14 20:10:08 -0500

    creating nav

--- components/atoms/Nav/Nav.jsx
+++ components/atoms/Nav/Nav.jsx
@@ -1,26 +1,36 @@
 import React from 'react';
 import Link from 'next/link';
+import MenuButton from '../MenuButton/MenuButton';
 import styles from './nav.module.css';
 
 const Nav = ({ user }) => {
-  user ? (
-    <ul>
-      <li className={styles.NavLink}>
-        <Link href="#">About</Link>
-      </li>
-      <li className={styles.NavLink}>
-        <Link href="#">Sign In</Link>
-      </li>
-    </ul>
-  ) : (
-    <ul>
-      <li className={styles.NavLink}>
-        <Link href="#">About</Link>
-      </li>
-      <li className={styles.NavLink}>
-        <Link href="#">Sign In</Link>
-      </li>
-    </ul>
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.navbutton}>
+        <MenuButton />
+      </div>
+      <div className={styles.navbar}>
+        {!user ? (
+          <ul>
+            <li className={styles.NavLink}>
+              <Link href="#">About</Link>
+            </li>
+            <li className={styles.NavLink}>
+              <Link href="#">Sign In</Link>
+            </li>
+          </ul>
+        ) : (
+          <ul>
+            <li className={styles.NavLink}>
+              <Link href="#">About</Link>
+            </li>
+            <li className={styles.NavLink}>
+              <Link href="#">Sign Out</Link>
+            </li>
+          </ul>
+        )}
+      </div>
+    </nav>
   );
 };
 
